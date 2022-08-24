b450501251be9378c46bbc45fc88068a408bbd38
sl1mslav <dm.partilov@gmail.com>
2022-08-24 12:17:46 +0500

    initial project settings and dependencies

---
+++ app/src/test/java/com/example/utvstories/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.example.utvstories
+
+import com.example.utvstories.domain.GetStoriesWithDateUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import org.junit.Test
+
+import org.junit.Assert.*
+import javax.inject.Inject
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
